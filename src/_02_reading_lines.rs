4591666704300a5824a7115a5ea3cfe8564e85d9
iceroliveira <ice_roliveira@uolinc.com>
2021-12-28 15:25:34 -0300

    fixing comments

--- src/_02_reading_lines.rs
+++ src/_02_reading_lines.rs
@@ -9,33 +9,44 @@ pub fn reading_lines() {
     let _my_string = String::new(); // we are not using this variable, so it has to be prefixed with _
 
     // ------------------------------------------------------------------
-    // the following is not possible,
-    // the programmer is trying to pass the whole string smart pointer ownership to the function
-    // the read_line function expects to receive a mutable reference to borrowed the string temporarily
-    // *borrowed*, because it doesnt want to be the owner of the string
-    // *mutable* reference, because it wants to change the string content
+    // the following is not possible:
+    // the programmer is trying to pass the whole string smart pointer ownership to the function.
+    // the read_line function expects to just receive a mutable reference to borrow the string temporarily.
+    // *borrowed*, because it doesnt want to be the owner of the string.
+    // *mutable* reference, because it wants to change the string content.
+    // so, a mutable borrow is a read-write reference to an object, without stealing the ownership
+    // of the object
 
-    // let result = stdin().read_line(my_string);
+    // let result = stdin().read_line(my_string); // impossible, as explained
 
-    // also not possible, since the reference is borrowed correctly (no ownership transferred),
-    // but the reference is immutable, so the function will not be able to change the content
+    // the following is also not possible.
+    // the reference is passed correctly (no ownership transferred),
+    // but it is immutable (read-only).
+    // The function will not be able to change the content.
     // let result = stdin().read_line(&my_string);
 
-    // also not possible, since the function expects a mutable smart pointer, and
+    // the following is also not possible,
+    // since the function expects a mutable smart pointer, and
     // the variable was declared as immutable
-    // let result = stdin().read_line(&mut my_string);
+    // let result = stdin().read_line(&mut _my_string);
 
-    // lets create a mutable reference here
+    // lets create a mutable variable, that owns an empty string here.
+    // this declaration makes the string a read-write object:
     let mut another_string = String::new();
 
-    another_string.as_str();
-    // not possible, since another string is mutable,
-    // but it is being passed as an immutable reference
-    // to a function that expects a mutable reference
-    //let result = stdin().read_line(&another_string);
+    // the following is not possible.
+    // 'another_string' is correctly declared as mutable (read-write),
+    // but it is being passed as an immutable reference (read-only)
+    // to a function that expects a mutable reference (read-write)
+    // let result = stdin().read_line(&another_string);
 
     println!("Now, lets read a string and assign to a string variable");
-    // the reference is a mutable borrowed reference, no ownership is transferred
+
+    // in the following line,
+    // the another_string is passed correctly as a mutably-borrowed reference.
+    // It means that it is a read-write reference to the string object,
+    // and the read_line function is NOT the owner of the string.
+    // rust is full of nuances, it is weird at first, but then you will get used to it
     let _result = stdin().read_line(&mut another_string);
 
     println!("string is: {}", another_string);
